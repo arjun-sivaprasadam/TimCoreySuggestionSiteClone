d1297b43a0aed9dc11c4dbaf695f2848823f34b5
ArjunSivaprasadam <asivaprasadam@geologicai.com>
2023-11-11 15:21:45 -0700

    Modding ConfigurationServices

--- SuggestionApp/SuggestionAppUI/Program.cs
+++ SuggestionApp/SuggestionAppUI/Program.cs
@@ -3,10 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
+builder.ConfigureServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
